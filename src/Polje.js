9c05c6a840bdb8092fe079ca0eb492826bb34b5c
unknown <am20170003@student.fon.bg.ac.rs>
2021-01-24 10:53:02 +0100

    Prvi commit

---
+++ src/Polje.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Polje.css';
+
+export default class Polje extends Component {
+    render() {
+        return (<div>
+            <div class="polje">
+
+            </div>
+        </div>
+        )
+    }
+}
